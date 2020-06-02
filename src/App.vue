0b44418fe41c6b7be72aa965399d660f50c66e17
r1ch <rich@bradi.sh>
2020-06-02 20:44:06 +0100

    moon times

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    {{ rise }}
+  </div>
+</template>
+
+<script>
+const SunCalc = require("suncalc");
+
+export default {
+  name: "App",
+  data: () => ({
+    word: "Hi!"
+  }),
+  computed: {
+    rise() {
+      return SunCalc.getMoonTimes(new Date(), 50, -4);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
